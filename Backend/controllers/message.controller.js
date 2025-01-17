558ada4c08ed04068c016cbbe852d154f89a9a43
joshiyash710 <joshiyash710@gmail.com>
2025-01-17 16:20:14 +0530

    refactor message handling; improve formatting in sendMessage function and add getMessage endpoint

--- Backend/controllers/message.controller.js
+++ Backend/controllers/message.controller.js
@@ -1,17 +1,17 @@
 import { Conversation } from "../models/conversation.model.js";
 import { Message } from "../models/message.model.js";
 
-export const sendMessage = async (req,res) => {
+export const sendMessage = async (req, res) => {
     try {
         const senderId = req.id;
         const receiverId = req.params.id;
         const { message } = req.body;
         let gotConversation = await Conversation.findOne({
-            participants : {$all : [senderId,receiverId]}
+            participants: { $all: [senderId, receiverId] }
         })
-        if(!gotConversation){
+        if (!gotConversation) {
             gotConversation = await Conversation.create({
-                participants : [senderId,receiverId]
+                participants: [senderId, receiverId]
             })
         }
         const newMessage = await Message.create({
@@ -19,18 +19,34 @@ export const sendMessage = async (req,res) => {
             receiverId,
             message
         })
-        if(newMessage){
+        if (newMessage) {
             gotConversation.messages.push(newMessage._id);
         }
         await gotConversation.save();
         return res.status(200).json({
-            message : "Message sent successfully !!!",
-            success : true
+            message: "Message sent successfully !!!",
+            success: true
         })
         //socket io 
 
     } catch (error) {
         console.log(error);
-        
+
+    }
+}
+
+export const getMessage = async (req, res) => {
+    try {
+        const receiverId = req.params.id
+        const senderId = req.id;
+        const conversation = await Conversation.findOne({
+            participants: { $all: [senderId, receiverId] }
+        }).populate("messages");
+        return res.status(200).json(conversation?.messages)
+
+
+    } catch (error) {
+        console.log(error);
+
     }
 }
--- Backend/routes/message.route.js
+++ Backend/routes/message.route.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { sendMessage } from '../controllers/message.controller.js';
+import { getMessage, sendMessage } from '../controllers/message.controller.js';
 import isAuthenticated from '../middleware/isAuthenticated.js';
 
 const router = express.Router()
 
 router.route('/send/:id').post(isAuthenticated,sendMessage)
+router.route('/get/:id').get(isAuthenticated,getMessage)
 
 export default router
